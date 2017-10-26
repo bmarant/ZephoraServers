a3aa0fdb4040ccb38b5afb95273d157ca4d45c4d
Bradford Matthew Arant II <barant2003@gmail.com>
2017-10-26 13:04:58 -0400

    First Commit

---
+++ ZephoConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZephoFoxer
+{
+    public class ZephoConnection
+    {
+        float xy;
+        float yz;
+        float zy;
+
+
+
+        public void PlayerPosition(float x, float y, float z)
+        {
+            xy = x;
+            yz = y;
+            zy = z;
+        }
+
+
+
+
+    }
+}
